aeab04828ed83857c8df5bc7bf73115691912ade
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-05 00:00:09 +0200

    create supports section

---
+++ components/Sections/Supports.tsx
@@ -0,0 +1,15 @@
+import Section from "../Section";
+import SectionHeader from "../SectionHeader";
+
+const Supports = () => {
+  return <Section>
+    <SectionHeader title="Our" titleHighlight="Supports" />
+    <ul>
+      <li>
+        <Image
+      </li>
+    </ul>
+  </Section>;
+};
+
+export default Supports;
