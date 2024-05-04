f9f9755b7a0d4c08c75d803e43b74163fc7ed6d0
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-04 13:48:19 +0200

    update content and css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="w-full">
-      <div className="px-4 md:px-10 py-4 xl:px-16 flex flex-row justify-between">
-        <Link href="/" className="flex flex-row gap-x-2 items-center">
+      <div className="px-4 md:px-10 py-4 xl:px-16 flex flex-row justify-center md:justify-between">
+        <Link href="/" className="hidden md:flex flex-row gap-x-2 items-center">
           <Car />
           <span className="font-bold">
             Moni<span className="text-primary">Post</span>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,19 @@
+import { Medal } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col h-full justify-center items-center text-center gap-y-4 mx-6 md:mx-6">
+    <div className="flex flex-col h-full justify-center items-center text-center gap-y-3 mx-6 md:mx-6">
+      <span className="flex flex-row px-4 py-2 rounded-full gap-x-2 items-center border font-semibold bg-yellow-500"><Medal className="size-12"/> N°1 Start Up Innovation </span>
       <h1>
         Moni<span className="text-primary">Post</span> helps world to <br /> <span className="bg-primary text-secondary p-2 rounded-lg"> drive safe.</span> 
       </h1>
       <p className="w-[300px] md:w-[540px]">
         Collect, analyze and make driving experience safer with postural data. We provide you a innovative way to create a safer driving experience. 
       </p>
-      <Button>Get MoniPost for free</Button>
+      <Button>Get MoniPost for free</Button>      
     </div>
   );
-};
+};    
 
 export default Hero;
