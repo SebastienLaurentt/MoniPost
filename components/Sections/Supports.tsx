4b92c130dd0884e3a1126aabbba054f5d49e6465
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-05 00:24:21 +0200

    update import and css

--- components/Sections/Supports.tsx
+++ components/Sections/Supports.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import EiffelLogo from "../../public/images/Eiffel.svg";
 import Lyon1Logo from "../../public/images/Lyon1.jpg";
-import UniversityLogo from "../../public/images/UniversityLyon.png";
+import UniversityLogo from "../../public/images/UniversityLyon.svg";
 import Section from "../Section";
 import SectionHeader from "../SectionHeader";
 
@@ -9,15 +9,15 @@ const Supports = () => {
   return (
     <Section>
       <SectionHeader title="Our" titleHighlight="Supports" />
-      <ul className="flex flex-row justify-center items-center gap-x-8 w-full mx-auto">
+      <ul className="flex flex-row justify-between items-center gap-x-4 md:gap-x-8 xl:gap-x-16 w-full px-4 md:px-8 xl:px-16">
         <li className="w-1/3">
-          <Image src={EiffelLogo} alt="University Eiffel Logo" />
+          <Image src={EiffelLogo} alt="University Eiffel Logo" className="w-full" />
         </li>
         <li className="w-1/3">
-          <Image src={Lyon1Logo} alt="University Lyon 1 Logo" />
+          <Image src={Lyon1Logo} alt="University Lyon 1 Logo" className="w-full" />
         </li>
         <li className="w-1/3">
-          <Image src={UniversityLogo} alt="University Lyon Logo" />
+          <Image src={UniversityLogo} alt="University Lyon Logo" className="w-full" />
         </li>
       </ul>
     </Section>
