4eb441861e8feb9af313afe52642c0093cb11da7
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-04 12:55:20 +0200

    update css

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="w-full">
-      <div className="px-4 md:px-10 xl:px-16">
+      <div className="px-4 md:px-10 py-4 xl:px-16 flex flex-row justify-between">
         <span>Logo</span>
         <nav>
-          <ul>
+          <ul className="flex flex-row gap-x-4">
             <li>
               <Link href="/">Home</Link>
             </li>
