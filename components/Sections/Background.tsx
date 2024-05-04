547247283afd9ac3f1731ab725d8a8eef188cf56
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-04 23:50:30 +0200

    create Background section

---
+++ components/Sections/Background.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Section from '../Section'
+import SectionHeader from '../SectionHeader'
+
+const Background = () => {
+  return (
+    <Section>
+      <SectionHeader title='Posture as a innovative' titleHighlight='measurement' />
+      <p>
+        Thank you for your interest in MoniPost. The privacy of our visitors is very important to us. This privacy policy describes the types of personal information we collect and how we use it.
+      </p>
+    </Section>
+  )
+}
+
+export default Background
