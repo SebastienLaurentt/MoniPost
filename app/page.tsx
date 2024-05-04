ce6d614ee6a54245e6d68a637633cc2c00c1f8d6
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-04 13:32:48 +0200

    update content and css

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="bg-gray-600 h-full flex flex-col items-center">
+    <main className=" h-full flex flex-col items-center">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col h-full justify-center items-center gap-y-2">
+    <div className="flex flex-col h-full justify-center items-center text-center gap-y-4 mx-6 md:mx-6">
       <h1>
-        Moni <span className="text-accent">Post</span> helps world to drive safe
+        Moni<span className="text-primary">Post</span> helps world to <br /> <span className="bg-primary text-secondary p-2 rounded-lg"> drive safe.</span> 
       </h1>
-      <p>
-        Collect, analyze and make driving experience safer with postural data
+      <p className="w-[300px] md:w-[540px]">
+        Collect, analyze and make driving experience safer with postural data. We provide you a innovative way to create a safer driving experience. 
       </p>
       <Button>Get MoniPost for free</Button>
     </div>
