110674de776c7e20860ee6b4e0b4ddea31db5b2e
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-05 00:02:25 +0200

    import supports img in supports section

--- components/Sections/Supports.tsx
+++ components/Sections/Supports.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
 import Section from "../Section";
 import SectionHeader from "../SectionHeader";
+import EiffelLogo from "../../public/images/Eiffel.png";
+import Lyon1Logo from "../../public/images/Lyon1.png";
+import UniversityLogo from "../../public/images/UniversityLogo.png";
 
 const Supports = () => {
   return <Section>
     <SectionHeader title="Our" titleHighlight="Supports" />
     <ul>
       <li>
-        <Image
+        <Image src={EiffelLogo} alt="Phone" width={24} height={24} />
+      </li>
+      <li>
+        <Image src={Lyon1Logo} alt="Phone" width={24} height={24} />
+      </li>
+      <li>
+        <Image src={UniversityLogo} alt="Phone" width={24} height={24} />
       </li>
     </ul>
   </Section>;
