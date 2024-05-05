a2d76ea19cf3a63d285f9b876c23984be852b4e6
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-06 00:28:44 +0200

    import price card in price and price in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Faq from "@/components/Sections/Faq";
+import Price from "@/components/Sections/Price";
 import Supports from "@/components/Sections/Supports";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <Supports />
+      <Price />
       <Faq />
     </main>
   );
--- components/Sections/Price.tsx
+++ components/Sections/Price.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Section from '../Section'
 import SectionHeader from '../SectionHeader'
+import PriceCard from '../PriceCard'
 
 const Price = () => {
   return (
     <Section marginTop={true} >
       <SectionHeader title="Choose your right" titleHighlight="plan" />
   
-      <ul>
-        
+      <ul className='flex flex-col md:flex-row md:gap-x-4 md:justify-center'>
+        <li>
+          <PriceCard forfait="Basic" forfaitDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit." price={0} />
+        </li>
+        <li>
+          <PriceCard forfait="Premium" forfaitDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. " price={19.99} />
+        </li>
+        <li>
+          <PriceCard forfait="Enterprise" forfaitDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit." price={39.99} />
+        </li>
       </ul>
     </Section>
   )
