c16856fb6f005c781d59baf2842a91443c17e531
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-06 00:15:14 +0200

    create price section

---
+++ components/Sections/Price.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Section from '../Section'
+import SectionHeader from '../SectionHeader'
+
+const Price = () => {
+  return (
+    <Section marginTop={true} >
+      <SectionHeader title="Choose your right" titleHighlight="plan" />
+  
+      <ul>
+        
+      </ul>
+    </Section>
+  )
+}
+
+export default Price
