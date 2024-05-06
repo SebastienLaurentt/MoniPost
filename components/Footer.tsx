4861bfc0a744a9a87df98f82aa8e1cd306a7c529
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-06 16:50:59 +0200

    update moniPost logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
-import { Car } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "../public/images/Logo.webp";
 
 const Footer = () => {
   return (
     <footer className="w-full">
       <div className="px-4 md:px-10 py-4 xl:px-16 flex flex-row justify-between">
         <Link href="/" className="hidden md:flex flex-row gap-x-2 items-center">
-          <Car />
+          <Image src={Logo} alt="Car" className="w-8" />
           <span className="font-bold">
             Moni<span className="text-primary">Post</span>
           </span>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
-import { Car } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "../public/images/Logo.webp";
 import { Button } from "./ui/button";
 
 const Header = () => {
@@ -7,7 +8,7 @@ const Header = () => {
     <header className="w-full">
       <div className="px-4 md:px-10 py-4 xl:px-16 flex flex-row justify-between">
         <Link href="/" className="flex flex-row gap-x-2 items-center">
-          <Car />
+          <Image src={Logo} alt="Car" className="w-8" />
           <span className="font-bold">
             Moni<span className="text-primary">Post</span>
           </span>
