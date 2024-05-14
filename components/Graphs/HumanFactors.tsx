b4d041ee0189f44e8691318b88cc1babdb22c397
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-14 23:30:37 +0200

    update humans factors graph

--- components/Graphs/HumanFactors.tsx
+++ components/Graphs/HumanFactors.tsx
@@ -4,12 +4,9 @@ import React from "react";
 import { Chart } from "react-google-charts";
 
 export const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
+  ["Causes", "Percentage"],
+  ["Humans", 90],
+  ["Other", 10],
 ];
 
 export const options = {
