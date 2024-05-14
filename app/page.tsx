d4c477c9cf47f4acc13188ef446ed4e8a2a746ad
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-15 00:28:22 +0200

    import dms in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Background from "@/components/Sections/Background";
+import Dms from "@/components/Sections/Dms";
 import Faq from "@/components/Sections/Faq";
 import PosturalArgument from "@/components/Sections/PosturalArgument";
 import Price from "@/components/Sections/Price";
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       {/* <Supports /> */}
       <Background />
+      <Dms />
       <PosturalArgument />
       <Price />
       <Faq />
