88d6afd34ab51a4e3014203edecd2fbb9563f9f2
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-15 00:38:17 +0200

    update content and css

--- components/Sections/Dms.tsx
+++ components/Sections/Dms.tsx
@@ -6,21 +6,18 @@ const Dms = () => {
   return (
     <Section marginTop={true}>
       <SectionHeader title="Current models lack" titleHighlight="precision" />
-      <div className="flex flex-col md:flex-row md:items-center gap-y-4 lg:px-12 xl:px-32">
-        <div className="md:w-1/2">
-          <p>
+      <div className="flex flex-col lg:flex-row lg:items-center gap-y-4 lg:gap-x-16  xl:px-32">
+        <div className="lg:w-3/4">
+          <p className="text-center lg:text-left text-sm md:text-md">
             Driver monitoring systems, or DMS, are emerging as a promising
             technology to assess a driver&apos;s attention and behavior while
-            driving, with potential intervention if needed. General safety
-            regulations and the EuroNCAP program promote widespread adoption of
-            DMS technology. According to a report, the DMS market is expected to
-            reach $4.72 billion by 2030. However, the performance of existing
-            DMS is unsatisfactory due to a lack of labeled data, particularly
-            postural data.
+            driving, with potential intervention if needed.  However, the performance of existing DMS is
+            unsatisfactory due to a lack of labeled data, particularly postural
+            data.
           </p>
         </div>
         <div>
-          <Image src="/images/DMS.png" alt="DMS" width={500} height={500} />
+          <Image src="/images/DMS.jpg" alt="DMS" width={1000} height={500} />
         </div>
       </div>
     </Section>
