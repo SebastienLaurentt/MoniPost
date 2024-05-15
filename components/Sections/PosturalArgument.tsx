5fac2001fa44deff45a5a51ad5cb4aa5002e6859
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-15 14:37:42 +0200

    update css

--- components/Sections/PosturalArgument.tsx
+++ components/Sections/PosturalArgument.tsx
@@ -7,8 +7,8 @@ const PosturalArgument = () => {
   return (
     <Section marginTop={true} marginBottom={true} dark={true}>
       <SectionHeader title="Postural data offers a" titleHighlight="better model" />
-      <div className="flex flex-col md:flex-row md:items-center gap-y-4  lg:gap-x-16 xl:px-32">
-        <div className="md:w-3/4">
+      <div className="flex flex-col lg:flex-row lg:items-center gap-y-4  lg:gap-x-16 xl:px-32">
+        <div className="lg:w-3/4">
           <p className="text-center lg:text-left text-sm md:text-md">
           To address the lack of data, we were able to establish a large-scale, high-quality annotated dataset within the vehicle&apos;s interior, leveraging 3D modeling. Through deep learning in artificial intelligence, we developed software aimed at estimating the driver&apos;s posture from an image, more accurately and reliably than the state of the art, to better understand the driver&apos;s condition.
           </p>
