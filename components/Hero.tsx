39942c6473b96f752486180d1fda48b36cbe4668
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-17 15:35:06 +0200

    update hero anim

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,14 +16,18 @@ const Hero = () => {
   useEffect(() => {
     const screenWidth = window.innerWidth;
     // Hero Text Animation
-    gsap.fromTo("#hero-title", { opacity: 0 }, { opacity: 1, duration: 1 });
+    gsap.fromTo("#hero-title", { opacity: 0, y: 20  }, { opacity: 1, y: 0,  duration: 1 });
     gsap.fromTo(
       "#hero-description",
-      { opacity: 0 },
-      { opacity: 1, duration: 1}
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0,  duration: 1 }
     );
-    gsap.fromTo("#hero-button", { opacity: 0 }, { opacity: 1, duration: 1 });
-    gsap.fromTo("#hero-partners", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.5 });
+    gsap.fromTo("#hero-button", { opacity: 0, y: 20  }, { opacity: 1, y: 0,  duration: 1 });
+    gsap.fromTo(
+      "#hero-partners",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 0.5 }
+    );
     gsap.fromTo(
       "#hero-introducing",
       { opacity: 0 },
@@ -34,7 +38,7 @@ const Hero = () => {
   return (
     <Section classname="flex flex-col pt-20 pb-20 items-center text-center bg-primary">
       <div className="flex flex-col items-center text-white">
-        <div id="hero-introducing">
+        <div id="hero-introducing" className="opacity-0">
           <AnimatedShinyText className="inline-flex items-center justify-center px-4  transition ease-out hover:text-neutral-600 hover:duration-300">
             <Shield className="mr-1 size-4 md:size-6" />
             <span className="text-xs md:text-sm"> Introducing MoniPost</span>
@@ -42,23 +46,23 @@ const Hero = () => {
           </AnimatedShinyText>
         </div>
         <div className="my-6 flex w-full flex-col justify-center">
-          <h1 id="hero-title" className="mb-3">
+          <h1 id="hero-title" className="mb-3 opacity-0">
             Enhance Driving Safety. <br /> Empowering Safer Journeys.
           </h1>
           <p
             id="hero-description"
-            className="mx-auto w-[340px] text-[14px] md:w-[540px] md:text-md"
+            className="mx-auto w-[340px] text-[14px] opacity-0 md:w-[540px] md:text-md"
           >
             MoniPost enhances driving safety through scientific analysis of
             postural data, offering an innovative way to create safer driving
             experiences.
           </p>
         </div>
-        <Button id="hero-button" className="font-bold">
+        <Button id="hero-button" className="font-bold opacity-0">
           <Link href="/#price">Get MoniPost</Link>
         </Button>
       </div>
-      <div id="hero-partners" className="mt-8 flex flex-col items-center">
+      <div id="hero-partners" className="mt-8 flex flex-col items-center opacity-0">
         <ul className="mb-12 flex w-[300px] flex-row items-center justify-between gap-x-4 md:w-[400px]">
           <li className="w-1/3">
             <Image
