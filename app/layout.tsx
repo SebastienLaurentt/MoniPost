4a0820af8bb6bff2874f14fa3bef3dba173851cd
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-18 15:31:32 +0200

    add metaData and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { incosolata } from "@/lib/font";
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "MoniPost - Enhance Driving Safety",
+  description:
+    "An innovative way to create safer driving experiences through scientific analysis of postural data",
+};
 
 export default function RootLayout({
   children,
@@ -10,6 +17,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#00a300" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${incosolata} flex flex-col `}>
         <Header />
         <div className="flex-1">{children}</div>
