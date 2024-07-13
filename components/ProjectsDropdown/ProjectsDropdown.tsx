a45ebeb5fe1367a1bbc4fe032c4e733d9a05531c
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-13 18:51:13 +0200

    update dropdown icon and css

--- components/ProjectsDropdown/ProjectsDropdown.tsx
+++ components/ProjectsDropdown/ProjectsDropdown.tsx
@@ -10,9 +10,22 @@ const ProjectsDropdown = () => {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="text-md text-white lg:hidden">
-        Menu
+        <svg
+          className="size-6 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="mr-2">
+      <DropdownMenuContent className="mr-2 md:mr-6">
         <DropdownMenuItem>
           <Link
             href="/#background-section"
@@ -22,26 +35,17 @@ const ProjectsDropdown = () => {
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link
-            href="/#models-section"
-            aria-label="Models section link"
-          >
+          <Link href="/#models-section" aria-label="Models section link">
             Solutions
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link
-            href="/#price-section"
-            aria-label="Prices section link"
-          >
+          <Link href="/#price-section" aria-label="Prices section link">
             Prices
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link
-            href="/#team-section"
-            aria-label="Team section link"
-          >
+          <Link href="/#team-section" aria-label="Team section link">
             Team
           </Link>
         </DropdownMenuItem>
